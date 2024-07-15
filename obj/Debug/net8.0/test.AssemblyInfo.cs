17461b51fa232faa015fc8924505d3325cd8c766
sks5995sks <sks5995study@gmail.com>
2024-07-15 11:25:28 +0900

    1일차 개발환경 및 깃 허브 업로드 완료

--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+910c2388769bcde757dcd57ce4f2fe5b3dbcb6bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
