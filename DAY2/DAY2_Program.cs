4fcb3d6414cb91e84c0f1231f85ff2c6ab5b91d4
sks5995sks <sks5995study@gmail.com>
2024-07-19 14:23:59 +0900

    1주차, 종합문제 (도전 문제 1, 2, 3)

--- DAY2/DAY2_Program.cs
+++
@@ -1,5 +0,0 @@
-// public class DAY2_Program{
-//     public static void Main(String[] args){
-
-//     }
-// }
--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9313fced346006ff3e546ffd624309471038b5f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afc4a401662640b180cc12acf47bbc904462fae3")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
