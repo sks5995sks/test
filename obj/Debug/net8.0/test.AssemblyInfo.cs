2833d247817bf9dd0527af4509d84c47ec8bec22
sks5995sks <sks5995study@gmail.com>
2024-07-22 10:23:27 +0900

    이름 변경

--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afc4a401662640b180cc12acf47bbc904462fae3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fcb3d6414cb91e84c0f1231f85ff2c6ab5b91d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
